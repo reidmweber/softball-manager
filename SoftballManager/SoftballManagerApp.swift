8b0d5c1a320b225d109d9f3a83e426c34a07a0f3
Reid Weber <reid.weber@wahoofitness.com>
2023-05-19 12:47:48 -0700

    Initial Commit

---
+++ SoftballManager/SoftballManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoftballManagerApp.swift
+//  SoftballManager
+//
+//  Created by Reid Weber on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct SoftballManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
